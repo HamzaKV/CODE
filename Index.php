8d2408941aa6e93c105dc8310ee1a8615d6a52c7
HamzaKV <hamzakv333@gmail.com>
2015-02-22 12:33:13 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,29 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Youth Employment</title>
+        <link type="text/css" href="CSS/StyleSheet.css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script type="text/javascript" src="JS/Script.js"></script>
+        <style>
+            body{margin: 0;}
+        </style>
+    </head>
+    <body style="background-color: #B9BAB8;" ondragstart="return false" >
+        <?php
+            include("Pages/Header.php")
+        ?>
+        <div>
+            <?php
+                include("Pages/Body.php")
+            ?>
+        </div>
+        <?php
+            include("Pages/Footer.php")
+        ?>
+    </body>
+</html>
