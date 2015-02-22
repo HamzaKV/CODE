46eba6811ddc18cfae11b130d115013d4bdccf1a
HamzaKV <hamzakv333@gmail.com>
2015-02-22 12:18:30 -0500

    Create Body.php

---
+++ Pages/Body.php
@@ -0,0 +1,18 @@
+<?php
+    
+?>
+<div id="changer" style="width: 60%; margin-left: auto; margin-right: auto">
+    <div id="write" style="padding-bottom: 30px; padding-top: 50px;">
+        <p>The objective of this application is to estimate the salary the user will expect
+        based on the parameters given as well list the number of jobs available and show
+        number of openings.</p>
+
+        <p>This application uses the data sets <a target="_blank" href="http://open.canada.ca/data/en/dataset/e80851b8-de68-43bd-a85c-c72e1b3a3890">
+        Summation of All Projected Demand Components, and COPS Occupational Groupings from Canadian Occupational Projection System (COPS) - 2013 
+        to 2022 projections</a> published by Labour force survey estimates (LFS), wages of employees 
+        by job permanence, union coverage, sex and age group, unadjusted for seasonality. Also
+        it uses <a target="_blank" href="http://open.canada.ca/data/en/dataset/2b169d70-5d9d-4c4a-849c-429ae5face4e">
+        Labour force survey estimates (LFS), wages of employees by job permanence, union 
+        coverage, sex and age group, unadjusted for seasonality</a> published by Statistics Canada.</p>
+    </div>
+</div>
