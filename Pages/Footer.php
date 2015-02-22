2304f53fd44accc5dbe51e16a287c03f1690aeff
HamzaKV <hamzakv333@gmail.com>
2015-02-22 12:18:54 -0500

    Update and rename Pages to Pages/Footer.php

---
+++ Pages/Footer.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<style>
+    #mobilesite:link {
+        color: #000;
+    }
+    #mobilesite:hover {
+        background-color: #fff;
+    }
+    #mobilesite:visited {
+        color: #000;
+    }
+</style>
+
+<div style="background-color: #373D33; color: #fff; height: 50px;">
+    <p style="font: 12px/50px Helvetica, sans-serif; text-align: center;">
+        Copyright &copy; 2015 www.varvaniweb.com
+    </p>
+</div>
