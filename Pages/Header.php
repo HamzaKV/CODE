dd2865e32413481f217eb4fddb23cc976a1ed6c3
HamzaKV <hamzakv333@gmail.com>
2015-02-22 12:19:18 -0500

    Update and rename Pages to Pages/Header.php

---
+++ Pages/Header.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<div style="height: 100px; background-color: #373D33; color: #fff;">
+    <div style="width: 60%; margin-left: auto; margin-right: auto">
+        <div style="float: left">
+            <p style="font: 40px/40px Helvetica, sans-serif;">
+                Youth Employment
+            </p>
+        </div>
+        <div style="float: right">
+            <p style="font: 16px/80px Helvetica, sans-serif;">
+                <a id="padlink" href="../Index.php">Home</a>
+                <a id="padlink" href="../Pages/search.php">Search</a>
+            </p>
+        </div>
+    </div>
+</div>
